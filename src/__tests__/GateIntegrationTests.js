e1142bed402e429b48d78d689496ea8449bb815c
jamespagedev <jamespagedev@outlook.com>
2019-01-31 00:25:17 -0700

    GateIntegration Testsuite complete | MVP Complete

--- src/__tests__/GateIntegrationTests.js
+++ src/__tests__/GateIntegrationTests.js
@@ -1,25 +1,20 @@
 // Test away!
 import React from 'react';
-import { render, fireEvent } from 'react-testing-library';
+import { render } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 import 'react-testing-library/cleanup-after-each';
-import Dashboard from '../dashboard/Dashboard.js';
-import Controls from '../controls/Controls.js';
+import Display from '../display/Display.js';
 
 /* Testsuite Gate Status:
   - Testset: Gate(Display) defaults to unlocked and open
     - Testcase: Gate is Unlocked by default
     - Testcase: Gate is Open by default
   - Testset: Gate cannot be opened if it is locked
-    - Testcase: 
-    - Testcase: 
-    - Testcase: 
-    - Testcase: 
+    - Note: This was already implemented in Controls.spec.js on Testset:
+            "the closed toggle button is disabled if the gate is locked"
   - Testset: Gate cannot be locked if gate is open
-    - Testcase: 
-    - Testcase: 
-    - Testcase: 
-    - Testcase: 
+    - Note: This was already implemented in Controls.spec.js on Testset:
+            "the locked toggle button is disabled if the gate is open"
 */
 
 // Testsuite
@@ -28,10 +23,18 @@ describe('Testsuite: Gate Status', () => {
   describe('Testset: Gate(Display) defaults to unlocked and open', () => {
     // Testcase
     it('Testcase: Gate is Unlocked by default', () => {
-      const { getByTestId } = render(<Controls />);
-      const button = getByTestId('closed');
+      const { getByTestId } = render(<Display />);
+      const openDiv = getByTestId('locked');
 
-      expect(button).toHaveTextContent(/Close Gate/i);
+      expect(openDiv).toHaveTextContent(/unlocked/i);
+    });
+
+    // Testcase
+    it('Testcase: Gate is Open by default', () => {
+      const { getByTestId } = render(<Display />);
+      const lockDiv = getByTestId('closed');
+
+      expect(lockDiv).toHaveTextContent(/open/i);
     });
   });
 });
